a91d42357e7e59d56b87c4f8a7ec2a14d178eef7
21120455 <hoai0455fit@gmail.com>
2024-04-14 16:28:50 +0700

    update websecurity

--- src/main/java/com/example/shopapp/configurations/WebSecurityConfig.java
+++ src/main/java/com/example/shopapp/configurations/WebSecurityConfig.java
@@ -40,14 +40,14 @@ public class WebSecurityConfig {
                             )
                             .permitAll()
                             //category request
-                            .requestMatchers(HttpMethod.GET,String.format("%s/categories**",apiPrefix)).hasAnyRole(Role.ADMIN,Role.USER)
+                            .requestMatchers(HttpMethod.GET,String.format("%s/categories**",apiPrefix)).permitAll()
                             .requestMatchers(HttpMethod.POST,String.format("%s/categories/**",apiPrefix)).hasRole(Role.ADMIN)
                             .requestMatchers(HttpMethod.PUT,String.format("%s/categories/**",apiPrefix)).hasRole(Role.ADMIN)
                             .requestMatchers(HttpMethod.DELETE,String.format("%s/categories/**",apiPrefix)).hasRole(Role.ADMIN)
                             //role
                             .requestMatchers(HttpMethod.GET,String.format("%s/roles",apiPrefix)).permitAll()
                             //product request
-                            .requestMatchers(HttpMethod.GET,String.format("%s/products**",apiPrefix)).hasAnyRole(Role.ADMIN,Role.USER)
+                            .requestMatchers(HttpMethod.GET,String.format("%s/products**",apiPrefix)).permitAll()
                             .requestMatchers(HttpMethod.POST,String.format("%s/products/**",apiPrefix)).hasRole(Role.ADMIN)
                             .requestMatchers(HttpMethod.PUT,String.format("%s/products/**",apiPrefix)).hasRole(Role.ADMIN)
                             .requestMatchers(HttpMethod.DELETE,String.format("%s/products/**",apiPrefix)).hasRole(Role.ADMIN)
